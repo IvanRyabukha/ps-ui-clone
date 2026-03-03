8155af51293d1495ca1e9b7b4d54e071bd2b20e4
Ivan Ryabukha <ryabukhaivan98@gmail.com>
2026-03-03 23:39:03 +0200

    fix img path

--- src/data/games.data.ts
+++ src/data/games.data.ts
@@ -5,9 +5,9 @@ export const GAMES_DATA = [
       'PlayStation Store',
     slug: 'playstation-store',
     coverImage:
-      '/images/covers/ps-store-cover.jpg',
+      'images/covers/ps-store-cover.jpg',
     bgImage:
-      '/images/backgrounds/playstation-store.jpg',
+      'images/backgrounds/playstation-store.jpg',
     platform: 'PS5',
   },
 
@@ -17,9 +17,9 @@ export const GAMES_DATA = [
       'Final Fantasy VII Remake',
     slug: 'final-fantasy-vii-remake',
     coverImage:
-      '/images/covers/ff-remake-cover.jpg',
+      'images/covers/ff-remake-cover.jpg',
     bgImage:
-      '/images/backgrounds/final-fantasy-vii-remake.jpg',
+      'images/backgrounds/final-fantasy-vii-remake.jpg',
     platform: 'PS5',
   },
   {
@@ -27,9 +27,9 @@ export const GAMES_DATA = [
       'Ghost of Tsushima',
     slug: 'ghost-of-tsushima',
     coverImage:
-      '/images/covers/ghost-of-tsushima-game.jpg',
+      'images/covers/ghost-of-tsushima-game.jpg',
     bgImage:
-      '/images/backgrounds/ghost-of-tsushima.jpg',
+      'images/backgrounds/ghost-of-tsushima.jpg',
     platform: 'PS5',
   },
   {
@@ -37,18 +37,18 @@ export const GAMES_DATA = [
       'Shadow of the Colossus',
     slug: 'shadow-of-the-colossus',
     coverImage:
-      '/images/covers/shadow-colossus-cover.jpg',
+      'images/covers/shadow-colossus-cover.jpg',
     bgImage:
-      '/images/backgrounds/shadow-of-the-colossus.jpg',
+      'images/backgrounds/shadow-of-the-colossus.jpg',
     platform: 'PS5',
   },
   {
     title: 'Bloodborne',
     slug: 'bloodborne',
     coverImage:
-      '/images/covers/bloodborne-cover.jpg',
+      'images/covers/bloodborne-cover.jpg',
     bgImage:
-      '/images/backgrounds/bloodborne.jpg',
+      'images/backgrounds/bloodborne.jpg',
     platform: 'PS4',
   },
   {
@@ -56,9 +56,9 @@ export const GAMES_DATA = [
       'God of War Ragnarok',
     slug: 'god-of-war-ragnarok',
     coverImage:
-      '/images/covers/god-of-war-ragnarok-2021.jpg',
+      'images/covers/god-of-war-ragnarok-2021.jpg',
     bgImage:
-      '/images/backgrounds/god-of-war-ragnarok.jpg',
+      'images/backgrounds/god-of-war-ragnarok.jpg',
     platform: 'PS5',
   },
   {
@@ -66,9 +66,9 @@ export const GAMES_DATA = [
       'The Last of Us Part II',
     slug: 'the-last-of-us-part-2',
     coverImage:
-      '/images/covers/tlou-cover.jpg',
+      'images/covers/tlou-cover.jpg',
     bgImage:
-      '/images/backgrounds/the-last-of-us-part-2.jpg',
+      'images/backgrounds/the-last-of-us-part-2.jpg',
     platform: 'PS4',
   },
   {
@@ -76,27 +76,27 @@ export const GAMES_DATA = [
       'Horizon Forbidden West',
     slug: 'horizon-forbidden-west',
     coverImage:
-      '/images/covers/horizon-forbidden-west.jpg',
+      'images/covers/horizon-forbidden-west.jpg',
     bgImage:
-      '/images/backgrounds/horizon-forbidden-west.jpg',
+      'images/backgrounds/horizon-forbidden-west.jpg',
     platform: 'PS5',
   },
   {
     title: 'Elden Ring',
     slug: 'elden-ring',
     coverImage:
-      '/images/covers/elden-ring-cover.jpg',
+      'images/covers/elden-ring-cover.jpg',
     bgImage:
-      '/images/backgrounds/elden-ring.jpg',
+      'images/backgrounds/elden-ring.jpg',
     platform: 'PS5',
   },
   {
     title: 'Death Stranding',
     slug: 'death-stranding',
     coverImage:
-      '/images/covers/death-standing-cover.jpg',
+      'images/covers/death-standing-cover.jpg',
     bgImage:
-      '/images/backgrounds/death-stranding.jpg',
+      'images/backgrounds/death-stranding.jpg',
     platform: 'PS4',
   },
   {
@@ -104,9 +104,9 @@ export const GAMES_DATA = [
       'Resident Evil 4 Remake',
     slug: 'resident-evil-4-remake',
     coverImage:
-      '/images/covers/resident-evil-4.jpg',
+      'images/covers/resident-evil-4.jpg',
     bgImage:
-      '/images/backgrounds/resident-evil-4-remake.jpg',
+      'images/backgrounds/resident-evil-4-remake.jpg',
     platform: 'PS5',
   },
 ];
